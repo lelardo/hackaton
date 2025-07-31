b7bae239badcac1473a7212322ce3281af35bf53
jcbastianl <jcbastianl@example.com>
2025-07-31 09:15:34 -0500

    Actualiza urls.py

--- competencia/app/urls.py
+++ competencia/app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ejemplo', views.Class_Ejemplo.as_view()),
+    path('ejemplo/<int:id>', views.Class_EjemploParametros.as_view()),
+    path('ejemplo-upload', views.Class_EjemploUpload.as_view()),
 ]
