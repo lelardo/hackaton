86aa7becf6a51f9800743bad1a2657238bd80a11
lelardo <leonardo.orozco@unl.edu.ec>
2025-07-31 09:38:08 -0500

    Mejoras

--- competencia/app/models.py
+++ competencia/app/models.py
@@ -4,6 +4,7 @@ class Closet(models.Model):
     name = models.CharField(max_length=255)
 
 
+
 class Drawer(models.Model):
     SIZE_CHOICES = [
         ('S', 'Small'),
@@ -18,7 +19,8 @@ class Drawer(models.Model):
     ]
     
     name = models.CharField(max_length=255)
-    max_capacity = models.IntegerField()
+    max_capacity = models.FloatField()
+    objects = models.ManyToManyField('Object', blank=True, related_name='drawers_as_list')
     size = models.CharField(
         max_length=1,
         choices=SIZE_CHOICES,
@@ -35,8 +37,32 @@ class Drawer(models.Model):
         related_name='drawers'
     )
 
+    def add_object(self, object):
+        if self.objects.count() < self.max_capacity:
+            self.objects.add(object)
+            object.drawer = self
+            object.save()
+        else:
+            raise ValidationError("El cajón ha alcanzado su capacidad máxima.")
+
+    def sort_objects(self):
+        self.objects.order_by('size')
+        self.save()
+
+
 class Object(models.Model):
-    identity = models.IntegerField()
+    TYPE_CHOICES = [
+        ('C', 'Clothes'),
+        ('P', 'Papers'),
+        ('W', 'Wires'),
+    ]
+    
+    type = models.CharField(
+        max_length=1,
+        choices=TYPE_CHOICES,
+        default='C'
+    )
+    size = models.FloatField()
     name = models.CharField(max_length=255)
     drawer = models.ForeignKey(
         Drawer,
@@ -53,5 +79,22 @@ class History(models.Model):
         related_name='histories'
     )
 
+    def create_register(self, type, object_name=None, objecto_count=None):
+        types = {
+            'D': 'borrado',
+            'O': 'ordenado',
+            'A': 'agregado',
+        }
+        if type == 'D' or type == 'A':
+            if objecto_count > 1:
+                self.description = f"Se ha {type} {objecto_count} objetos {object_name} en el cajón {self.drawer.name}"
+            else:
+                self.description = f"Se ha {type} un objeto {object_name} en el cajón {self.drawer.name}"
+        else:
+            self.description = f"Se ha {type} el cajón {self.drawer.name}"
+        
+        self.save()
+
     
     
+
