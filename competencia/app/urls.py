9e02e7c8bfb4210fcf5f087097066f016724e144
lelardo <leonardo.orozco@unl.edu.ec>
2025-07-31 10:37:59 -0500

    UrlS LISTOS

--- competencia/app/urls.py
+++ competencia/app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from . import views
 from .views import *
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recommendation/<int:drawer_id>/', views.RecommendationView.as_view(), name='recommendation'),
+    path('objects/create/', ObjectCreateView.as_view(), name='object-create'),
+    path('objects/assign/', ObjectAssignView.as_view(), name='object-assign'),
+    path('objects/remove-from-drawer/', ObjectRemoveFromDrawerView.as_view(), name='object-remove-from-drawer'),
+    path('drawers/sort-objects/', DrawerSortObjectsView.as_view(), name='drawer-sort-objects'),
+    path('drawers/remove-duplicates/', DrawerRemoveEqualObjectsView.as_view(), name='drawer-remove-duplicates'),
 
-    #path('ejemplo', views.Class_Ejemplo.as_view()),
-    #path('ejemplo/<int:id>', views.Class_EjemploParametros.as_view()),
-    #path('ejemplo-upload', views.Class_EjemploUpload.as_view()),
 
 ]
