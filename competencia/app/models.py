23b0b38a658f1e3a94143251106a10d89c64898a
lelardo <leonardo.orozco@unl.edu.ec>
2025-07-31 09:09:35 -0500

    models

--- competencia/app/models.py
+++ competencia/app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class Closet(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Drawer(models.Model):
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    ]
+
+    TYPE_CHOICES = [
+        ('C', 'Clothes'),
+        ('P', 'Papers'),
+        ('W', 'Wires'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    max_capacity = models.IntegerField()
+    size = models.CharField(
+        max_length=1,
+        choices=SIZE_CHOICES,
+        default='M'
+    )
+    type = models.CharField(
+        max_length=1,
+        choices=TYPE_CHOICES,
+        default='C'
+    )
+    closet = models.ForeignKey(
+        Closet,
+        on_delete=models.CASCADE,
+        related_name='drawers'
+    )
+
+class Object(models.Model):
+    identity = models.IntegerField()
+    name = models.CharField(max_length=255)
+    drawer = models.ForeignKey(
+        Drawer,
+        on_delete=models.CASCADE,
+        related_name='objects'
+    )
+
+class History(models.Model):
+    description = models.CharField(max_length=255)
+    date = models.DateField()
+    drawer = models.ForeignKey(
+        Drawer,
+        on_delete=models.CASCADE,
+        related_name='histories'
+    )
+
+    
+    
